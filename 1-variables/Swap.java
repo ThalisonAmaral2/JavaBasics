356b2521b13f76853cb2e6df3ac593f5df8f33d7
ThalisonAmaral2 <thalisonamaral2@gmail.com>
2023-06-09 14:40:27 -0300

    Swap Variables

---
+++ 1-variables/Swap.java
@@ -0,0 +1,16 @@
+public class Swap {
+    public static void main(String[] args){
+        String first = "black";
+        String second = "white";
+
+        System.out.println("First: "+first+" Second: "+second);
+
+        String swap = second;
+        second = first;
+        first = swap;
+
+        System.out.println("First: "+first+" Second: "+second);
+
+
+    }
+}
